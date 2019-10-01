d07bbaee2357a00aed33dc19d725c4ee2a18629d
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-01 03:41:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,10 +5,10 @@ consolidated_cart = {}
     name = item.keys[0]
 
     consolidated_cart[name] = item.values[0]
-    if consolidated_cart[item_name][:count] 
-    consolidated_cart[item_name][:count] += 1
+    if consolidated_cart[name][:count] 
+    consolidated_cart[name][:count] += 1
     else
-      consolidated_cart[item_name][:count] = 1
+      consolidated_cart[name][:count] = 1
     end
   end
   consolidated_cart
