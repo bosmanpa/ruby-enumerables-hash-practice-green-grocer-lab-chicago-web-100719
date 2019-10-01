fed2e41661adf8bf0bb502afc1ba88099713e99c
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-01 03:32:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,20 @@ def apply_coupons(cart, coupons)
       name = coupon[:item]
       
       if cart[name] && cart[name][:count] >= coupon:num]
- end
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[name][:clearance], :count => 1} 
+        end 
+  
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+end 
+  cart 
+end
+
 
 def apply_clearance(cart)
   # code here
