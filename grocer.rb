a6aaab4df476347cbe5e86e5bdc50ebbc777c50b
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-01 03:26:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,11 +1,10 @@
 def consolidate_cart(cart) 
   new_cart = {} 
   cart.each do |items_array| 
-    items_array.each do |item, attribute_hash| 
-      new_cart[item] ||= attribute_hash 
-      if new_cart[item][:count] == TRUE
-      new_cart[item][:count] += 1 
-      else new_cart[item][:count] = 1 
+    items_array.each do |item, attribute| 
+      new_cart[item] ||= attribute 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
   end 
 end 
 new_cart 
