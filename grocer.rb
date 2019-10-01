b30b04acba48cf793fad99b1d1ab63551a870dd7
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-01 03:38:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,10 +2,9 @@ def consolidate_cart(cart)
 consolidated_cart = {}
 
   cart.each do |item|
-    item_name = item.keys[0]
+    name = item.keys[0]
 
     consolidated_cart[item_name] = item.values[0]
-
     if consolidated_cart[item_name][:count] 
       consolidated_cart[item_name][:count] += 1
     else
